0bc5b396b893ec037f0d25ce20895192daa976dc
Rishu Garg <rishugarg102@gmail.com>
2019-06-15 15:23:13 +0530

    Camera Intent

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.quiz
 
+import android.content.Intent
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-
+private var cameraButton:Button?=null
+    private var galleryButton:Button?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+                cameraButton = findViewById<Button>(R.id.buttonC)
+                galleryButton = findViewById(R.id.button9)
+
+        cameraButton?.setOnClickListener(View.OnClickListener {
+            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(cameraIntent,1)
+        })
+
+        }
 /*Toast.makeText(this,"Override onCreate Method Called",Toast.LENGTH_SHORT).show()
 
         val myPlant:Plant=Plant("","","","","","",0,0)    //calling constructor
@@ -23,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         myPlant.plantName="Wadas Memory Magnolia"
               var a =         myPlant.plantName*/
 
-        var flower = Plant()  //object1
+      /*  var flower = Plant()  //object1
         var Tree = Plant()    // object 2
 
         var collectionOfPlants:ArrayList<Plant> = ArrayList()
@@ -31,8 +44,10 @@ class MainActivity : AppCompatActivity() {
         collectionOfPlants.add(flower)
         Toast.makeText(this,"Flower object added",Toast.LENGTH_SHORT).show()
         collectionOfPlants.add(Tree)
-        Toast.makeText(this,"Object 2 added",Toast.LENGTH_SHORT).show()
-    }
+        Toast.makeText(this,"Object 2 added",Toast.LENGTH_SHORT).show()*/
+
+
+
 
 
 
