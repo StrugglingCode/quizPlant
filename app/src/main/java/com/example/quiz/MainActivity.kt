32f8bb9d6b2c7daab10caac4375f82b666a93c2c
Rishu Garg <rishugarg102@gmail.com>
2019-06-15 14:58:44 +0530

    Collections

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -11,18 +11,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-Toast.makeText(this,"Override onCreate Method Called",Toast.LENGTH_SHORT).show()
+/*Toast.makeText(this,"Override onCreate Method Called",Toast.LENGTH_SHORT).show()
+
+        val myPlant:Plant=Plant("","","","","","",0,0)    //calling constructor
 
 
-        val myPlant:Plant=  Plant("Koelreuteria","paniculata","","Golden Rain Tree",
+      /*  Plant("Koelreuteria","paniculata","","Golden Rain Tree",
             "Koelreuteria_paniculata_branch.JPG",
-            "Branch of Koelreuteria paniculata",3,24)
-        //calling constructor
+            "Branch of Koelreuteria paniculata",3,24)*/
+
+        myPlant.plantName="Wadas Memory Magnolia"
+              var a =         myPlant.plantName*/
+
+        var flower = Plant()  //object1
+        var Tree = Plant()    // object 2
+
+        var collectionOfPlants:ArrayList<Plant> = ArrayList()
+
+        collectionOfPlants.add(flower)
+        Toast.makeText(this,"Flower object added",Toast.LENGTH_SHORT).show()
+        collectionOfPlants.add(Tree)
+        Toast.makeText(this,"Object 2 added",Toast.LENGTH_SHORT).show()
     }
 
 
 
-    inner class downloadinPlantTask:AsyncTask<String,Int,List<Plant>>()   //AsyncTask is doing heavy process in the background
+    inner class downloadingPlantTask:AsyncTask<String,Int,List<Plant>>()   //AsyncTask is doing heavy process in the background
     {
         override fun doInBackground(vararg params: String?): List<Plant>?  //vararg kind of array    ?MEANS OPTIONAL
         {
--- app/src/main/java/com/example/quiz/Plant.kt
+++ app/src/main/java/com/example/quiz/Plant.kt
@@ -1,6 +1,20 @@
 package com.example.quiz
 
-class Plant(genus:String, species:String, cultivar:String, common:String, pictureName:String, description:String,difficulty:Int,id:Int=0)
+class Plant(genus:String, species:String, cultivar:String,
+            common:String, pictureName:String, description:String,difficulty:Int,id:Int=0) //primary Constructor
+
+
 {
 
+    constructor():this("","","","","","",0,0) //this refers to primary constructor
+private var _plantName:String?=null      //instance Variable
+
+    var plantName:String?               //another variable of same name as instance variable
+
+    get() = _plantName
+
+    set(value)
+    {
+        _plantName = value
+    }
 }
